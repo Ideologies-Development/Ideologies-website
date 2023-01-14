0db1512e1ca5a9cff2ffdacea14ac863b77d1989
Commander R <wmxavier2004@gmail.com>
2023-01-14 17:42:34 +0100

    Changed the port so it matched the one of the hosting

--- backend.js
+++ backend.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = 4000;
+const port = 25557;
 
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'static')));
